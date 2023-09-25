dc11b2ae787b667444036e39511c8a269c9edee2
Eymen Efe Altun <eymenefealtun@hotmail.com>
2023-09-25 22:36:07 +0300

    Update C#.cs

--- how_to_use/C#.cs
+++ how_to_use/C#.cs
@@ -1,3 +1,9 @@
+// First way:
+
+// Download LexiExtract via NuGet package manager or 'dotnet add package LexiExtract'
+
+// Second way:
+
 //1- Import the txt file into your project.
 //2- Change its 'Build Action' to Embedded resource and its 'Copy to Output Directory' to Copy if newer from its properties.
 //3- Assign to a string array using method below;
